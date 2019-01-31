7a62c6c3012c27b321eb00b653d9df12ea544021
jianyinlin <jianyinlin@Ctrip.com>
2019-01-31 18:51:50 +0800

    修改表字段大小写

---
+++ src/main/java/org/b3log/symphony/model/Code.java
@@ -0,0 +1,8 @@
+package org.b3log.symphony.model;
+
+/**
+ * Created by jianyinlin on 2019/1/31.
+ */
+public class Code {
+
+}
---
+++ src/main/java/org/b3log/symphony/model/Detail.java
@@ -0,0 +1,10 @@
+package org.b3log.symphony.model;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * Created by jianyinlin on 2019/1/31.
+ */
+public class Detail {
+
+}
